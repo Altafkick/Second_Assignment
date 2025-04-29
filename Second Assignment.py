d9b34737dd69bec364198a51ed8434c2f6f76332
Altafkick <altafhhebbal@gmail.com>
2025-04-30 00:27:14 +0530

    Add files via upload

---
+++ Second Assignment.py
@@ -0,0 +1,10 @@
+Number1=int(input("Enter The Number :"))
+if Number1%2==0:
+    print(f"{Number1} is Even")
+else:
+    print(f"{Number1} is  Odd")
+print("Thank you")
+
+for i in range(1,51):
+    print(i)
+print("The sum of numbers from 1 to 51 is",sum(range(1,51)))
